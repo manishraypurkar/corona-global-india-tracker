ca9f29a0b28ea25ed4db4399c8f0f4a36f18f814
Manish raypurkar <35144263+manishraypurkar@users.noreply.github.com>
2020-06-04 21:47:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+include 'main.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->	
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Font Family -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/7b5c10d17a.js" crossorigin="anonymous"></script>
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+
+    <title>Covid 19 Tracker</title>
+</head>
+<body>
+<div class="container-fluid bg-light p-5 text-center my-3 ">
+    <h1>Global Covid-19 Tracker</h1>
+    <h5 class="lead">Track the cases..!</h5>
+<a href ="statewise.php">INDIA TRACKER</a>
+</div>
+
+<div class="container my-5 ">
+    <div class="row text-center">
+        <div class="col-4 text-warning">
+            <h5>Confirmed</h5>
+            <?php echo $total_confirmed;?>
+        </div>
+        <div class="col-4 text-success">
+            <h5>Recovered</h5>
+            <?php echo $total_recovered;?>
+        </div>
+        <div class="col-4 text-danger">
+            <h5>Deceased</h5>
+            <?php echo $total_deaths;?>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="table-responsive">
+    <table class="table table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Countries</th>
+                <th scope="col">Confirmed</th>
+                <th scope="col">Recovered</th>
+                <th scope="col">Deceased</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                foreach($data as $key => $value){
+                    $increase = $value[$days_count]['confirmed'] - $value[$days_count_prev]['confirmed']
+            ?>
+                <tr>
+                    <th><?php echo $key;?></th>
+                    <td>
+                        <?php echo $value[$days_count]['confirmed'];?>
+                        <?php if($increase !=0){?>
+                            <small class="text-danger pl-2"><i class="fas fa-arrow-up"></i><?php echo $increase;?></small>
+                        <?php } else{?>
+<small class="text-danger pl-2"><i class="fas fa-arrow-up"></i><?php echo $increase;?></small>
+ <?php }?>
+                     
+                    </td>
+                    <td>
+                        <?php echo $value[$days_count]['recovered'];?>
+                    </td>
+                    <td>
+                        <?php echo $value[$days_count]['deaths'];?>
+                    </td>
+                </tr>
+
+                <?php }?>
+        </tbody>
+    </table>
+    </div>
+</div>
+</div>
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container text-center">
+    <span class="text-muted">MADE BY <a href="https://manishraypurkar.github.io/portfolio%20project/index.html" target="_blank">MANISH ANIL RAYPURKAR</a></span>
+<p class="text-muted">Data Sources:
+<a href="https://api.covid19india.org/data.json">https://api.covid19india.org</a>
+<a href="https://pomber.github.io/covid19/timeseries.json">https://pomber.github.io/covid19/timeseries.json</a>
+</p>
+  </div>
+</footer>
+    
+</body>
+</html>
---
+++ statewise.php
@@ -0,0 +1,103 @@
+      <?php 
+        $statedata = file_get_contents('https://api.covid19india.org/data.json');
+        // $sumdata = file_get_content('https://api.covid19api.com/summary');
+        //  echo ($sumdata);
+        $livedata = json_decode($statedata, true);
+       $numstates = count($livedata['statewise']);
+//while($i=0)
+//{
+//	$conf= $livedata['statewise'][$i]['confirmed'];
+//	$act= $livedata['statewise'][$i]['active'];
+//	$rec= $livedata['statewise'][$i]['recovered'];
+//	$det= $livedata['statewise'][$i]['deaths'];
+//}
+	
+ 
+    ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>corona HINT</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+  
+    <link href="https://fonts.googleapis.com/css2?family=Arvo:wght@700&family=Dosis:wght@400;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet">
+  </head>
+  <body>
+<div class="container-fluid bg-light p-5 text-center my-3">
+ <h1>Total Cases in INDIA</h1>
+</div>
+
+
+<div class="container my-5 ">
+    <div class="row text-center">
+        <div class="col-3 text-warning">
+            <h5>Confirmed</h5>
+            <?php echo $livedata['statewise'][0]['confirmed'];?>
+        </div>
+	<div class="col-3 text-info">
+            <h5>Active</h5>
+            <?php echo $livedata['statewise'][0]['active'];?>
+        </div>	
+        <div class="col-3 text-success">
+            <h5>Recovered</h5>
+            <?php echo $livedata['statewise'][0]['recovered'];?>
+        </div>
+        <div class="col-3 text-danger">
+            <h5>Deceased</h5>
+            <?php echo $livedata['statewise'][0]['deaths'];?>
+        </div>
+    </div>
+</div>
+ 
+	<div class="container-fluid">		     
+      <div class="table-responsive">
+          <table class="table">
+           <thead class="thead-dark">
+            <tr>
+              <th scope="col">Last updated time</th>
+              <th scope="col">State</th>
+              <th scope="col">Confirmed</th>
+              <th  scope="col">Active</th>
+              <th scope="col">Recovered</th>
+              <th scope="col">Deaths</th>
+	</tr>
+	</thead>
+<?php
+	$i=1;
+       while($i<$numstates){
+?>
+    <tr>
+        <td> <?php echo $livedata['statewise'][$i]['lastupdatedtime'] ?></td>
+        <td> <?php echo $livedata['statewise'][$i]['state'] ?></td>
+        <td > <?php echo $livedata['statewise'][$i]['confirmed'] ?></td>
+        <td > <?php echo $livedata['statewise'][$i]['active']  ?></td>
+        <td > <?php echo $livedata['statewise'][$i]['recovered'] ?></td>
+        <td> <?php echo $livedata['statewise'][$i]['deaths'] ?></td>
+    </tr>
+
+    <?php
+        $i++;
+        }
+
+    ?>
+
+          </table>
+      <footer class="footer mt-auto py-3 bg-light">
+  <div class="container text-center">
+    <span class="text-muted">MADE BY <a href="https://manishraypurkar.github.io/portfolio%20project/index.html">MANISH ANIL RAYPURKAR</a></span>
+<p class="text-muted">Data Sources:
+<a href="https://api.covid19india.org/data.json">https://api.covid19india.org</a>
+<a href="https://pomber.github.io/covid19/timeseries.json">https://pomber.github.io/covid19/timeseries.json</a>
+</p>
+  </div>
+</footer>
+  </body>
+</html>
